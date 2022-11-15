9d94db42d3cf1ebf0d46d6e02a6be88e6587d4a4
KarimataNaoki <kn0109azay@gmail.com>
2022-11-15 17:12:14 +0900

    超入門講座

---
+++ .history/object_20221115171006.rb
@@ -0,0 +1,21 @@
+# p 10
+
+# p 10.div(5)
+
+# p 10.remainder(3)
+
+# p 10.class
+
+# p 10.0.class
+
+# p "Good morning".index("d")
+
+# p "Good Morning".length
+
+# p "Good Morning".class
+
+p (10>1)
+p (10>1).class
+
+p (10<1)
+p (10<1).class
